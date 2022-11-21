277ab7fa3747bd84dc00e611992ab43525cd9662
unknown <tgavkhare80@gmail.com>
2022-11-21 10:55:52 +0530

    Ready to deploy

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ from cv2 import Mat
 import pytesseract
 import numpy as np
 import re
+import glob
+import numpy as np
+from PIL import Image
+import re
 
 
 # path to tesseract executable
@@ -123,6 +127,113 @@ class Image_Processing:
             x, y, w, h = roi[0]
             roi_img = self.img[y : y + h, x : x + w]
             cv2.imwrite(f"{path}/{x}.jpg", roi_img)
+
+    def number_plate_detection(self):
+        def clean2_plate(plate):
+            gray_img = cv2.cvtColor(plate, cv2.COLOR_BGR2GRAY)
+        
+            _, thresh = cv2.threshold(gray_img, 110, 255, cv2.THRESH_BINARY)
+            if cv2.waitKey(0) & 0xff == ord('q'):
+                pass
+            num_contours,hierarchy = cv2.findContours(thresh.copy(),cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        
+            if num_contours:
+                contour_area = [cv2.contourArea(c) for c in num_contours]
+                max_cntr_index = np.argmax(contour_area)
+        
+                max_cnt = num_contours[max_cntr_index]
+                max_cntArea = contour_area[max_cntr_index]
+                x,y,w,h = cv2.boundingRect(max_cnt)
+        
+                if not ratioCheck(max_cntArea,w,h):
+                    return plate,None
+        
+                final_img = thresh[y:y+h, x:x+w]
+                return final_img,[x,y,w,h]
+        
+            else:
+                return plate,None
+        
+        def ratioCheck(area, width, height):
+            ratio = float(width) / float(height)
+            if ratio < 1:
+                ratio = 1 / ratio
+            if (area < 1063.62 or area > 73862.5) or (ratio < 3 or ratio > 6):
+                return False
+            return True
+        
+        def isMaxWhite(plate):
+            avg = np.mean(plate)
+            if(avg>=115):
+                return True
+            else:
+                return False
+        
+        def ratio_and_rotation(rect):
+            (x, y), (width, height), rect_angle = rect
+        
+            if(width>height):
+                angle = -rect_angle
+            else:
+                angle = 90 + rect_angle
+        
+            if angle>15:
+                return False
+        
+            if height == 0 or width == 0:
+                return False
+        
+            area = height*width
+            if not ratioCheck(area,width,height):
+                return False
+            else:
+                return True
+        
+        
+        img2 = cv2.GaussianBlur(self.img, (5,5), 0)
+
+        cv2.imshow("Image of car ",img2)
+        cv2.waitKey(1000)
+        cv2.destroyAllWindows()
+
+        img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+        cv2.imshow("Image of car ",img2)
+        cv2.waitKey(1000)
+        cv2.destroyAllWindows()
+        
+        img2 = cv2.Sobel(img2,cv2.CV_8U,1,0,ksize=3)
+        cv2.imshow("Image of car ",img2)
+        cv2.waitKey(1000)
+        cv2.destroyAllWindows()	
+        _,img2 = cv2.threshold(img2,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+        
+        element = cv2.getStructuringElement(shape=cv2.MORPH_RECT, ksize=(17, 3))
+        morph_img_threshold = img2.copy()
+        cv2.morphologyEx(src=img2, op=cv2.MORPH_CLOSE, kernel=element, dst=morph_img_threshold)
+        cv2.imshow("Image of car ",img2)
+        cv2.waitKey(1000)
+        cv2.destroyAllWindows()
+        num_contours, hierarchy= cv2.findContours(morph_img_threshold,mode=cv2.RETR_EXTERNAL,method=cv2.CHAIN_APPROX_NONE)
+        cv2.drawContours(img2, num_contours, -1, (0,255,0), 1)
+        cv2.imshow("Image of car ",img2)
+        cv2.waitKey(1000)
+        cv2.destroyAllWindows()
+        
+        
+        for i,cnt in enumerate(num_contours):
+            min_rect = cv2.minAreaRect(cnt)
+            if ratio_and_rotation(min_rect):
+                x,y,w,h = cv2.boundingRect(cnt)
+                plate_img = self.img[y:y+h,x:x+w]
+                if(isMaxWhite(plate_img)):
+                    clean_plate, rect = clean2_plate(plate_img)
+                    if rect:
+                        fg=0
+                        x1,y1,w1,h1 = rect
+                        x,y,w,h = x+x1,y+y1,w1,h1
+                        plate_im = Image.fromarray(clean_plate)
+                        text = pytesseract.image_to_string(plate_im, lang='eng')
+                        return text
 
 
 # img_rez = cv2.resize(img, None, fx=0.5, fy=0.5)
--- run.py
+++ run.py
@@ -1,12 +1,12 @@
 import sys
-from streamlit import cli as stcli
+from streamlit.web import cli as stcli
 import streamlit as st
 from streamlit_app import main
 
 
 if __name__ == "__main__":
-    if st._is_running_with_streamlit:
+    # if st._is_running_with_streamlit:
         main()
-    else:
-        sys.argv = ["streamlit", "run", sys.argv[0]]
-        sys.exit(stcli.main())
+    # else:
+    #     sys.argv = ["streamlit", "run", sys.argv[0]]
+    #     sys.exit(stcli.main())
